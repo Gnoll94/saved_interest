0395e3514db74421e6a32d6700a52d25691bb039
Nolan <Gnoll94@gmail.com>
2020-03-05 22:35:04 -0500

    Adding Monthly Payment Calculations

--- src/App.js
+++ src/App.js
@@ -3,19 +3,38 @@ import {startOfToday, format, addMonths, differenceInMonths} from 'date-fns'
 
 export const App = () => {
 
-    const [loanPrincipal, setLoanPrincipal] = useState();
-    const [loanInterest, setLoanInterest] = useState();
-    const [loanInterestRate, setLoanInterestRate] = useState();
+    const [loanPrincipal, setLoanPrincipal] = useState(0);
+    const [loanInterest, setLoanInterest] = useState(0);
+    const [loanInterestRate, setLoanInterestRate] = useState(0);
     const [loanLength, setLoanLength] = useState();
     const [payOffDate, setPayOffDate] = useState(format(startOfToday(), 'yyyy-MM-dd'));
     const [loanTotalInterest, setLoanTotalInterest] = useState(0.00);
     const [loanEarlyMonths, setLoanEarlyMonths] = useState(0);
+    const [monthlyPayment, setMonthlyPayment] = useState();
 
     const calculateTotal = () => {
+        const loanTotal = (loanPrincipal + loanInterest);
+        console.log("Loan Principal", loanPrincipal);
+        console.log("Loan Interest", loanInterest);
+        console.log("Loan Total", loanTotal);
+        console.log("Loan Length", loanLength);
+
+        const convertedRate = loanInterestRate * .01 / 12;
+        console.log("Converted Rate", convertedRate);
+
+        const convertedWithLoan = Math.pow(1 + convertedRate, loanLength);
+        console.log("Converted With Loan", convertedWithLoan);
+
+        setMonthlyPayment(Number(loanTotal / ((convertedWithLoan - 1) / (convertedRate * (convertedWithLoan)))));
+        console.log(monthlyPayment.toFixed(2));
+
         const fullyPaidOffDate = addMonths(startOfToday(), loanLength);
         console.log(fullyPaidOffDate);
+
         setLoanEarlyMonths(differenceInMonths(fullyPaidOffDate, new Date(payOffDate)));
         setLoanTotalInterest(Number((loanInterestRate / loanLength) * (loanPrincipal)).toFixed(2));
+
+        console.log(Number((loanInterestRate / differenceInMonths(fullyPaidOffDate, new Date(payOffDate))) * (loanPrincipal)).toFixed(2));
     };
 
     return (
@@ -28,7 +47,7 @@ export const App = () => {
             placeholder="Enter Loan Amount"
             type="number"
             value={loanPrincipal}
-            onChange={e => setLoanPrincipal(e.target.value)}
+            onChange={e => setLoanPrincipal(Number(e.target.value))}
           />
         </div>
       </div>
@@ -40,7 +59,7 @@ export const App = () => {
             placeholder="Enter Loan Interest"
             type="number"
             value={loanInterest}
-            onChange={e => setLoanInterest(e.target.value)}
+            onChange={e => setLoanInterest(Number(e.target.value))}
           />
         </div>
       </div>
@@ -52,7 +71,7 @@ export const App = () => {
             placeholder="Enter Loan Interest Rate"
             type="number"
             value={loanInterestRate}
-            onChange={e => setLoanInterestRate(e.target.value)}
+            onChange={e => setLoanInterestRate(Number(e.target.value))}
           />
         </div>
       </div>
@@ -64,7 +83,7 @@ export const App = () => {
             placeholder="Enter # of Loan Payments Remaining"
             type="number"
             value={loanLength}
-            onChange={e => setLoanLength(e.target.value)}
+            onChange={e => setLoanLength(Number(e.target.value))}
           />
         </div>
       </div>
