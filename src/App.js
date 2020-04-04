56bf88ef53232b1ed90714391a78e9fe346f17ff
Nolan <Gnoll94@gmail.com>
2020-04-04 18:12:18 -0400

    Moving around to new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {startOfToday, format, addMonths, differenceInMonths} from 'date-fns'
-
-const roundNumber = (number, places) => parseFloat(number.toFixed(places))
+import {AmortizationTable, createAmortizationSchedule} from "./components/AmortizationTable";
+import {Input} from "./components/Input";
 
 export const App = () => {
 
@@ -9,50 +9,21 @@ export const App = () => {
     const [loanInterestRate, setLoanInterestRate] = useState(8.40);
     const [loanLength, setLoanLength] = useState(72);
     const [payOffDate, setPayOffDate] = useState(format(startOfToday(), 'yyyy-MM-dd'));
-    const [loanTotalInterest, setLoanTotalInterest] = useState(0.00);
-    const [loanEarlyMonths, setLoanEarlyMonths] = useState(0);
-    const [monthlyPayment, setMonthlyPayment] = useState();
+    const [finishOnPaymentNumber, setFinishOnPaymentNumber] = useState();
+    const [amortizationSchedule, setAmortizationSchedule] = useState({});
+    const [savedInterest, setSavedInterest] = useState(0.00);
 
-    const calculateTotal = () => {
+    const onClick = () => {
+        const [currentSavedInterest, loanTable, paymentIndex] = createAmortizationSchedule(loanInterestRate, loanAmount, loanLength, payOffDate);
+        setAmortizationSchedule(loanTable);
+        setSavedInterest(currentSavedInterest);
+        setFinishOnPaymentNumber(String(paymentIndex));
+    };
 
-        const convertedRate = (loanInterestRate * .01) / 12;
-        console.log("Converted Rate", convertedRate);
-
-        const firstInterestAmount = ((loanInterestRate * .01) / 12) * loanAmount;
-        console.log("First Interest Amount", firstInterestAmount);
-
-        console.log("Loan Amount", loanAmount);
-        console.log("Loan Length", loanLength);
-
-        const convertedWithLoan = Math.pow(1 + convertedRate, loanLength);
-        console.log("Converted With Loan", convertedWithLoan);
-
-        const monthlyPayment = Math.round(Number(loanAmount / ((convertedWithLoan - 1) / (convertedRate * (convertedWithLoan)))));
-        console.log("Monthly Payment", monthlyPayment);
-        const loanTable = {};
-
-        loanTable[0] = {
-            startingBalance: loanAmount,
-            repayment: monthlyPayment,
-            interestPaid: firstInterestAmount,
-            principalPaid: (monthlyPayment - firstInterestAmount),
-            newBalance: loanAmount - (monthlyPayment - firstInterestAmount)
-        };
-
-        for (let i=1; i < loanLength; i++){
-            const loanBalance = loanTable[i-1].newBalance;
-            const interestPayment = ((loanInterestRate * .01) / 12) * loanBalance;
-            const roundedInterest = Math.round(interestPayment * 100) / 100;
-            loanTable[i] = {
-                startingBalance: loanTable[i-1].newBalance,
-                repayment: monthlyPayment,
-                interestPaid: roundedInterest,
-                principalPaid: Math.round((monthlyPayment - roundedInterest) * 100) / 100,
-                newBalance: Math.round((loanBalance - (monthlyPayment - roundedInterest)) * 100) / 100
-            }
-        }
-        console.log(loanTable);
-
+    const tableProps = {
+        amortizationSchedule,
+        savedInterest,
+        finishOnPaymentNumber
     };
 
     return (
@@ -107,22 +78,10 @@ export const App = () => {
       </div>
       <div className="row">
         <div className="col-12 text-center">
-          <button onClick={calculateTotal}>Compute Saved Interest</button>
+          <button onClick={onClick}>Compute Saved Interest</button>
         </div>
       </div>
-        <div className="row">
-            <div className="col-12 text-center">
-                <div>Loan Total Interest</div>
-                {loanTotalInterest}
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12 text-center">
-                <div>Loan Being Paid Off</div>
-                {loanEarlyMonths}
-                <div>Months Early</div>
-            </div>
-        </div>
+        <AmortizationTable {...tableProps} />
     </div>
     );
 };
---
+++ src/components/AmortizationTable.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {addMonths, differenceInMonths, startOfToday} from "date-fns";
+
+export const createAmortizationSchedule = (loanInterestRate, loanAmount, loanLength, payOffDate) => {
+
+    // Interest & Payment Calculations
+    const convertedRate = (loanInterestRate * .01) / 12;
+    const firstInterestAmount = Math.round(((loanInterestRate * .01) / 12) * loanAmount * 100) / 100;
+    const convertedWithLoan = Math.pow(1 + convertedRate, loanLength);
+    const monthlyPayment = Math.round(Number(loanAmount / ((convertedWithLoan - 1) / (convertedRate * (convertedWithLoan)))) * 100) / 100;
+    const fullyPaidOffDate = addMonths(startOfToday(), loanLength);
+
+    const paymentIndex = loanLength - differenceInMonths(fullyPaidOffDate, new Date(payOffDate));
+
+    const loanTable = [];
+
+    loanTable[0] = {
+        startingBalance: loanAmount,
+        repayment: monthlyPayment,
+        interestPaid: firstInterestAmount,
+        principalPaid: (monthlyPayment - firstInterestAmount),
+        newBalance: loanAmount - (monthlyPayment - firstInterestAmount)
+    };
+
+    let currentSavedInterest = 0.00;
+
+    for (let i=1; i < loanLength; i++){
+        const loanBalance = loanTable[i-1].newBalance;
+        const interestPayment = ((loanInterestRate * .01) / 12) * loanBalance;
+        const roundedInterest = Math.round(interestPayment * 100) / 100;
+        if(i >= paymentIndex) {
+            currentSavedInterest += roundedInterest
+        }
+        loanTable[i] = {
+            startingBalance: loanTable[i-1].newBalance,
+            repayment: monthlyPayment,
+            interestPaid: roundedInterest,
+            principalPaid: Math.round((monthlyPayment - roundedInterest) * 100) / 100,
+            newBalance: i === loanLength - 1 ? 0.00 : Math.round((loanBalance - (monthlyPayment - roundedInterest)) * 100) / 100
+            // If last payment, set newBalance to 0.00 to finish loan
+        }
+    }
+
+    return [currentSavedInterest, loanTable, paymentIndex];
+};
+
+export const AmortizationTable = (props) => {
+    const {amortizationSchedule, finishOnPaymentNumber, savedInterest} = props;
+    const numberFormat = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+    const renderTableHeader = () => (
+        <tr id='Header'>
+            <th>Month</th>
+            <th>Starting Balance</th>
+            <th>Repayment</th>
+            <th>Interest Paid</th>
+            <th>Principal Paid</th>
+            <th>New Balance</th>
+        </tr>
+    );
+
+    const renderTableBody = () => {
+        return Object.keys(amortizationSchedule).map((month, index) => {
+            const { startingBalance, repayment, interestPaid, principalPaid, newBalance } = amortizationSchedule[month];
+            return (
+                <tr key={index} className={Number(month) >= Number(finishOnPaymentNumber) ? 'finish-month' : ''}>
+                    <td>{index+1}</td>
+                    <td>{numberFormat.format(startingBalance)}</td>
+                    <td>{numberFormat.format(repayment)}</td>
+                    <td>{numberFormat.format(interestPaid)}</td>
+                    <td>{numberFormat.format(principalPaid)}</td>
+                    <td>{numberFormat.format(newBalance)}</td>
+                </tr>
+            )
+        });
+    };
+
+    const renderTable = () => {
+        return (
+            <table className='table' id='months'>
+                <tbody>
+                {renderTableHeader()}
+                {renderTableBody()}
+                </tbody>
+            </table>
+        )
+    };
+
+    return (
+        <div className="row">
+        {Object.keys(amortizationSchedule).length > 0 && (
+            <div className="col-12 text-center">
+                <h3>Saved Interest Amount: {numberFormat.format(savedInterest)}</h3>
+                <h1>Amortization Schedule</h1>
+                {renderTable()}
+            </div> )
+        }
+        </div>
+    )
+};
