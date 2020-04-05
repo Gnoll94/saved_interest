c43205c0a76c62258e1c60e5328b9d13933255d7
Nolan <Gnoll94@gmail.com>
2020-04-04 23:01:32 -0400

    Updates

---
+++ precache-manifest.727f9d3422f7c18ac4ae32fb3d1f80e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d41e48ee5f1ba875cb303dd555da2f2",
+    "url": "/saved_interest/index.html"
+  },
+  {
+    "revision": "83ab26eacd52a1eecc1c",
+    "url": "/saved_interest/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0aa90ba5e661e88158ec",
+    "url": "/saved_interest/static/css/main.26eac737.chunk.css"
+  },
+  {
+    "revision": "83ab26eacd52a1eecc1c",
+    "url": "/saved_interest/static/js/2.99db4f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saved_interest/static/js/2.99db4f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa90ba5e661e88158ec",
+    "url": "/saved_interest/static/js/main.dbd91e3c.chunk.js"
+  },
+  {
+    "revision": "4f0706056cba4528a570",
+    "url": "/saved_interest/static/js/runtime-main.28a3bd90.js"
+  }
+]);
